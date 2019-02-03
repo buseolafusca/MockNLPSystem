af6fd8c0c5cea3ca91896df8ff0e45612214c0cb
Adebusola Adegbuyi <busseolafusca@gmail.com>
2019-02-03 12:01:23 +0000

    Created an endpoint to return random barchart and piechart data

---
+++ application.py
@@ -0,0 +1,30 @@
+import random
+
+import requests
+import json
+import os
+from flask import Flask, Response
+
+application = app = Flask(__name__)
+
+
+@app.route('/')
+def accept_question():
+
+    data_path = ""
+    file_index = random.randint(0, 1)
+    folder = os.path.dirname(os.path.abspath(__file__))
+    if file_index == 1:
+        data_path = os.path.join(folder, 'barchart.json')
+    elif file_index == 0:
+        data_path = os.path.join(folder, 'piechart.json')
+
+    with open(data_path) as file:
+        suggested_charts = json.loads(file.read())
+
+    index = random.randint(0, len(suggested_charts)-1)
+    return Response(response=json.dumps(suggested_charts[index]), content_type='application/json')
+
+
+if __name__ == '__main__':
+    application.run()
