4688bfd04b6cb14791efa211c573203ba1e01a39
Jesse <446346099@qq.com>
2019-02-14 00:51:49 +0000

    Update application.py

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ application = app = Flask(__name__)
 def accept_question():
 
     data_path = ""
-    file_index = random.randint(0, 2)
+    file_index = random.randint(0, 3)
     folder = os.path.dirname(os.path.abspath(__file__))
     if file_index == 0:
         data_path = os.path.join(folder, 'barchart.json')
@@ -19,6 +19,8 @@ def accept_question():
         data_path = os.path.join(folder, 'piechart.json')
     elif file_index == 2:
         data_path = os.path.join(folder, 'linechart.json')
+    elif file_index == 3:
+        data_path = os.path.join(folder, 'mutiplelinechart.json')
 
     with open(data_path) as file:
         suggested_charts = json.loads(file.read())
