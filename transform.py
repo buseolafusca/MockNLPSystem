67db4cfe13f6805d98f99d22593160a65e094b0a
Adebusola Adegbuyi <busseolafusca@gmail.com>
2019-02-19 19:56:46 +0000

    started work on the sample questions and answers

---
+++ transform.py
@@ -0,0 +1,19 @@
+continuous_dict = {"name": "", "datatype": "integer", "measuretype": "continuous", "measuresubtype": "ratio"}
+categorical_dict = {"name": "A&E Diagnosis", "datatype": "string", "measuretype": "categorical", "measuresubtype": "nominal"}
+
+response_dict = {
+    "responseMsg": "SUCCESS",
+    "responseCode": "00",
+    "responseData": {
+      "summary": {
+        "question": "",
+        "row_count": 4,
+        "col_count": 5
+      },
+      "fields": [],
+      "body": []
+    }
+}
+
+
+
